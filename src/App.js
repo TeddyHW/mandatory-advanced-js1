a2747cf7ee647ed7ccd84ae4b76b847112c2d490
Teddy Wendt <teddy97@live.se>
2020-02-14 12:00:00 +0100

    första, startad

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div class="main">
+        <div class="login">
+
+        </div>
+        <div class="chat">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
